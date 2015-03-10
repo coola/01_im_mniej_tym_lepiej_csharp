0e0123dcd88a3829d45b0859e43fa310f4ca31a5
coola <coola@o2.pl>
2015-03-10 19:09:15 +0100

    first commit

---
+++ 01_im_mniej_tym_lepiej_csharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _01_im_mniej_tym_lepiej_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
