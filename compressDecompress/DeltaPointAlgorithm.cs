fb9e461934eb7bcd4bde44039293bbc8628b46be
coola <coola@o2.pl>
2015-03-14 18:33:16 +0100

    change name of the folder

---
+++ compressDecompress/DeltaPointAlgorithm.cs
@@ -0,0 +1,17 @@
+﻿using common;
+
+namespace compress
+{
+    public class DeltaPointAlgorithm : PointAlgorithm
+    {
+        public override string CompressPoint(GPS_Point line)
+        {
+            return string.Format("{0}|{1}|{2}|{3}", line.GetLatitudeLeftSide, line.GetLatitudeRightSide, line.GetLongitudeLeftSide, line.GetLongitudeRightSide);
+        }
+
+        public override GPS_Point DecompressPoint(string point)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
